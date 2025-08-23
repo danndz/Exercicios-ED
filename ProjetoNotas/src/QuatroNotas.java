9c86ed7775a24bfbeb76a2a1a0eabfc8eb584de6
Danilo Dias <danilodiass910@gmail.com>
2025-08-22 21:18:58 -0300

    Adicionando Exercicio

--- ProjetoNotas/src/QuatroNotas.java
+++ ProjetoNotas/src/QuatroNotas.java
@@ -2,13 +2,13 @@ public class QuatroNotas {
     public static void main(String[] args) {
         
         int[] notas = {5, 3, 6, 8};
-        int soma = 0;
+        int num = 0;
 
         for (int i = 0; i < notas.length; i++) {
-            soma += notas[i];
+            num += notas[i];
         }
 
-        int media = soma / notas.length;
+        int media = num / notas.length;
         System.out.println("A média das notas é: " + media);
     }
 }
